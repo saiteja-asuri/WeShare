9b4276ea82a5f6a574c9076ea1f0c4c138cc508e
saiteja-asuri <asuri.saiteja0712@gmail.com>
2023-06-29 00:13:21 +0530

    intial commit

---
+++ src/main/java/com/weshare/app/WeShareappApplication.java
@@ -0,0 +1,14 @@
+package com.weshare.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeShareappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeShareappApplication.class, args);
+		
+	}
+
+}
